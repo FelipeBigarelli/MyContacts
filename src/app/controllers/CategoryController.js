fccbe78bae193d40f258f0fab10f98e48fb3f6b3
felipebigarellidantasdacosta <felipe.costa@g4tech.com.br>
2021-12-21 14:42:17 -0300

    MyContacts -> Added PostgreSQL

---
+++ src/app/controllers/CategoryController.js
@@ -0,0 +1,50 @@
+const CategoriesRepository = require('../repositories/CategoriesRepository');
+
+class CategoryController {
+  async index(request, response) {
+    const categories = await CategoriesRepository.findAll();
+
+    response.json(categories);
+  }
+
+  async store(request, response) {
+    const { name } = request.body;
+
+    if (!name) {
+      return response.status(400).send({ error: 'Name is required' });
+    }
+
+    const category = await CategoriesRepository.create({ name });
+
+    response.json(category);
+  }
+
+  async update(request, response) {
+    const { id } = request.params;
+    const { name } = request.body;
+
+    const findCategory = await CategoriesRepository.findById(id);
+
+    if (!findCategory) {
+      return response.status(404).send({ error: 'User not found' });
+    }
+
+    if (!name) {
+      return response.status(404).send({ error: 'Name is required' });
+    }
+
+    const category = await CategoriesRepository.update(id, { name });
+
+    response.json(category);
+  }
+
+  async delete(request, response) {
+    const { id } = request.params;
+
+    await CategoriesRepository.delete(id);
+
+    response.sendStatus(204);
+  }
+}
+
+module.exports = new CategoryController();
